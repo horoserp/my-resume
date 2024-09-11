f67dcbb2275444f41e5b2b456aa021fdc1677f60
Robert Horosewski <horoserp@yahoo.com>
2024-09-11 08:55:29 -0400

    Update: Add form that handles skill changes.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Wait for the document to load
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", changeSkills);
+})
+
+// Update the listed skills on submit
+function changeSkills(e) {
+    e.preventDefault();
+    const skills = document.getElementsByClassName("skill");
+    const inputs = document.getElementsByTagName("input");
+    for (let key in skills) {
+        skills[key].innerHTML = inputs[key].value;
+    }
+}
